1c9417de06b030dec01aa5e4e96c1d6e4e6e2457
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-08-02 12:34:39 +0100

    add PATCH reviews by id model

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -11,3 +11,19 @@ exports.selectReviewsByID = (review_id) => {
         }
     })
 }
+
+exports.updateReviewsByID = (review_id, reviewPatch) => {
+    const {inc_votes} = reviewPatch
+    if (inc_votes === undefined) {
+        return Promise.reject({status: 400, msg: "Bad request"})
+    }
+    return db.query("UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;", [inc_votes, review_id])
+    .then((result) => {
+        if (result.rows[0] === undefined) {
+            return Promise.reject({status: 404, msg: "Review does not exist"})
+        }
+        else {
+            return result.rows[0]
+        }
+    })
+}
