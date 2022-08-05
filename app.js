758bce7cc1c4bfc6dac58e69c84ae071cd46d6b6
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-08-05 15:39:59 +0100

    add app.js updated for DELETE comments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 
 const { getCategories } = require("./controllers/categories.controller");
+const { deleteCommentById } = require("./controllers/comments.controller");
 const { getReviewsByID, patchReviewsByID, getReviews, getCommentsByID, postCommentsByID } = require("./controllers/reviews.controller");
 const { getUsers } = require("./controllers/users.controller");
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors");
@@ -14,6 +15,7 @@ app.get("/api/users", getUsers);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id/comments", getCommentsByID);
 app.post("/api/reviews/:review_id/comments", postCommentsByID);
+app.delete('/api/comments/:comment_id', deleteCommentById);
 
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
