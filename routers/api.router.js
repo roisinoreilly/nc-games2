ae88bb2b3aab4722df99be88e98dceb6775cecf5
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-12-09 12:01:25 +0000

    update users router

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,10 +1,12 @@
 const apiRouter = require("express").Router()
-const { getCategories } = require("../controllers/categories.controller")
+const categoriesRouter = require("./categories.router")
 const commentsRouter = require("./comments.router")
 const reviewsRouter = require("./reviews.router")
+const usersRouter = require("./users.router")
 
 apiRouter.use("/reviews", reviewsRouter)
 apiRouter.use("/comments", commentsRouter)
-apiRouter.use("/categories", getCategories)
+apiRouter.use("/categories", categoriesRouter)
+apiRouter.use("/users", usersRouter)
 
 module.exports = apiRouter
